57218ffda6e9f94a3d636aa20658270588585cbf
root <root@srv1512828.hstgr.cloud>
2026-04-25 17:25:11 +0000

    fix: load prop-types before recharts in dashboard

--- src/marketing_dashboard.py
+++ src/marketing_dashboard.py
@@ -36,6 +36,7 @@ def render_marketing_dashboard() -> str:
 
   <script crossorigin src="https://unpkg.com/react@18.2.0/umd/react.production.min.js"></script>
   <script crossorigin src="https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js"></script>
+  <script src="https://unpkg.com/prop-types@15.8.1/prop-types.min.js"></script>
   <script src="https://unpkg.com/recharts@2.12.7/umd/Recharts.js"></script>
   <script src="https://unpkg.com/@babel/standalone@7.25.7/babel.min.js"></script>
   <script type="text/babel" data-presets="env,react">
